2d112203de71dd6fac151a43620bf990b989ab3c
Julie <j.prunaret@gmail.com>
2023-10-31 17:36:32 +0100

    add - installation of Chakra icons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,27 @@
-import { List, ListItem } from '@chakra-ui/react';
+import { AtSignIcon, CalendarIcon, EditIcon } from '@chakra-ui/icons';
+import { List, ListIcon, ListItem } from '@chakra-ui/react';
 import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
     <List color="white" fontSize="1.2em" spacing={4}>
       <ListItem>
-        <NavLink tp="/">Dashboard</NavLink>
+        <NavLink to="/">
+          <ListIcon as={CalendarIcon} color="white" />
+          Dashboard
+        </NavLink>
       </ListItem>
       <ListItem>
-        <NavLink tp="/">New Task</NavLink>
+        <NavLink to="/">
+          <ListIcon as={EditIcon} color="white" />
+          New Task
+        </NavLink>
       </ListItem>
       <ListItem>
-        <NavLink tp="/">Profile</NavLink>
+        <NavLink to="/">
+          <ListIcon as={AtSignIcon} color="white" />
+          Profile
+        </NavLink>
       </ListItem>
     </List>
   );
